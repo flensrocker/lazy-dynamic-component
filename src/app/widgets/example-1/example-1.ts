047980d48a64f6471c21d8bec956e8a0102df996
Lars Hanisch <lars.hanisch@googlemail.com>
2026-02-03 18:47:19 +0100

    WIP: add selector to widgets to avoid conflicts

--- src/app/widgets/example-1/example-1.ts
+++ src/app/widgets/example-1/example-1.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  selector: 'ldc-example-1',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `Content of Example 1`,
 })
--- src/app/widgets/example-2/example-2.ts
+++ src/app/widgets/example-2/example-2.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  selector: 'ldc-example-2',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `Content of Example 2`,
 })
